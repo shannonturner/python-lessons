d98bf72c364af04b86f51b78110ba6e9d6d5db35
Shannon Turner <shannon.v.turner@gmail.com>
2014-04-05 15:01:36 -0400

    Added lesson 4 deduplicate.py

---
+++ playtime/lesson04_deduplicate.py
@@ -0,0 +1,15 @@
+# Challenge level: Beginner
+
+# Scenario: You have two files containing a list of email addresses of people who attended your events.
+#       File 1: People who attended your Film Screening event
+#       https://github.com/shannonturner/python-lessons/blob/master/section_09_(functions)/film_screening_attendees.txt
+#
+#       File 2: People who attended your Happy hour
+#       https://github.com/shannonturner/python-lessons/blob/master/section_09_(functions)/happy_hour_attendees.txt
+#
+
+# Note: You should create functions to accomplish your goals.
+
+# Goal 1: You want to get a de-duplicated list of all of the people who have come to your events.
+
+# Goal 2: Who came to *both* your Film Screening and your Happy hour?
