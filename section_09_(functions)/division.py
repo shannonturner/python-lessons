b979f41bb0eac1a14efc33728aa2fe7318733074
Shannon Turner <shannon.v.turner@gmail.com>
2014-04-05 08:36:59 -0400

    Changed division.py to highlight the implicit None value returned

--- section_09_(functions)/division.py
+++ section_09_(functions)/division.py
@@ -1,4 +1,4 @@
 def division(x, y):
     if y == 0:
-        return None # You can't divide by Zero! If you do, you'll get an error.
+        return # You can't divide by Zero! If you do, you'll get an error.
     return x/y # It's implied that this only happens when y != 0, since otherwise the function would have ended at line 3
