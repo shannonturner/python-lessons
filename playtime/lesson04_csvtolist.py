f2aa80c23eeeea36844c770b83303363d51fc731
Shannon Turner <shannon.v.turner@gmail.com>
2014-04-06 10:15:09 -0400

    Added csvtolist exercise for playtime

---
+++ playtime/lesson04_csvtolist.py
@@ -0,0 +1,3 @@
+# Challenge level: Beginner
+
+# Goal: Using the code from Lesson 3: File handling and dictionaries, create a function that will open a CSV file and return the result as a nested list.
