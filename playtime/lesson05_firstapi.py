56ba2401701918e90db16ff807f826d3e4bce3f4
Shannon Turner <shannon.v.turner@gmail.com>
2014-05-02 19:36:07 -0400

    Added Lesson 5 playtime: first api

---
+++ playtime/lesson05_firstapi.py
@@ -0,0 +1,19 @@
+# Exercise: Using your first API
+
+# API documentation: http://bechdeltest.com/api/v1/doc
+
+# Goal 1:
+#   Ask the user for the movie title they want to check
+#   Display all of the details about the movie returned by the API
+#
+#   Things to keep in mind:
+#       How will your program behave when multiple movies are returned?
+#       How will your program behave when no movies are returned?
+#       How will your program behave with works like "the" in the title?
+
+# Goal 2:
+#   Check to see if the user input is a movie title or an ImdbID and use the proper endpoint
+
+# Goal 3:
+# Integrate this with the Open Movie Database API: http://www.omdbapi.com/
+#   Display all of the details from both APIs when searching for a movie.
