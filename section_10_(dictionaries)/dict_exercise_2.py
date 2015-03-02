7df6d495b972f1f0a42ba8a382d2bffc50f46aa1
Shannon Turner <shannon.v.turner@gmail.com>
2015-03-02 16:30:27 -0500

    Added second exercises -- adding items to an existing dictionary, looping through dictionaries

---
+++ section_10_(dictionaries)/dict_exercise_2.py
@@ -0,0 +1,30 @@
+contacts = {
+    "Hear Me Code": {
+        "twitter": "@hearmecode",
+        "github": "https://github.com/hearmecode"
+    },
+    "Shannon Turner": {
+        "twitter": "@svt827",
+        "github": "https://github.com/shannonturner"
+    },
+}
+
+# How to add a new item to an existing dictionary:
+contacts["Aliya Rahman"] = {
+    "twitter": "@AliyaRahman",
+    "github": "https://github.com/aliyarahman"
+}
+
+# Exercise 1: Add a new dictionary item to contacts for each person at your table.
+#   Rather than editing lines 1-10 above, add new entries to the contacts dictionary below.
+#   Keep in mind some people may not have a twitter account, and that's okay!
+
+# Exercise 2: Loop through the contacts dictionary to display everyone's contact information.
+#   Your output should look like this:
+
+# Hear Me Code's info: 
+#     twitter: @hearmecode
+#     github: https://github.com/hearmecode
+# Shannon Turner's info: 
+#     twitter: @svt827
+#     github: https://github.com/shannonturner
