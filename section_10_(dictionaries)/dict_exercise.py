70f9d8e0582ba8350d44c1d644412fbcbe25cf60
Shannon Turner <shannon.v.turner@gmail.com>
2015-03-02 13:05:55 -0500

    Added exercise for Lesson 3 to give more practice with nested access / slicing

---
+++ section_10_(dictionaries)/dict_exercise.py
@@ -0,0 +1,33 @@
+schools = {
+    "geometry": {
+        "coordinates": [
+            -81.50572799999999, 
+            39.21675500000001
+        ], 
+        "type": "Point"
+    }, 
+    "properties": {
+        "address": "300 Campus Drive, Parkersburg, WV 26104", 
+        "marker-color": "#3F3040", 
+        "marker-symbol": "circle", 
+        "name": "West Virginia University at Parkersburg"
+    }, 
+    "type": "Feature"
+}
+
+# Example Question: How could you "slice" the dictionary to print "Feature" from line 15?
+# Answer: print schools["type"]
+
+# Question 1: What slice will give you a dictionary 
+#   with the key of "coordinates" and a value containing a list
+#   of two decimal numbers?
+
+# Question 2: What slice will give you the address of the school?
+
+# Question 3: What slice will give you the name of the school?
+
+# Question 4: What slice will give you the latitude of the school? 
+#   (Hint: the latitude is 39.216...)
+
+# Question 5 (bonus): What slice will give you the marker-color 
+#   without the hashtag in front?
