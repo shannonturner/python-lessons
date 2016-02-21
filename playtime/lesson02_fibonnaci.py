01075e80d43c484d391ea70f8333b54c9d739ef3
Shannon Turner <shannon.v.turner@gmail.com>
2016-02-21 12:51:29 -0500

    Added Fizzbuzz and Fibonnaci

---
+++ playtime/lesson02_fibonnaci.py
@@ -0,0 +1,13 @@
+# Fibonnaci Sequence
+
+# Difficulty: Advanced
+
+# NOTE: This is *not* a practical example of how you'll use coding in your day to day, BUT it does appear on a surprising number of in-person coding interviews.  Why employers rely on something so impractical to gauge your coding ability is beyond me.
+#       But since they're using it, maybe in this narrow sense it's a practical exercise to know how to do after all.
+
+
+# The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by adding up the two numbers before it.
+
+# Create a program that will generate the first 10 numbers in the Fibonacci Sequence.
+
+# When completed, your program should have the output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
---
+++ playtime/lesson02_fizzbuzz.py
@@ -0,0 +1,128 @@
+# Fibonnaci Sequence
+
+# Difficulty: Beginner
+
+# NOTE: This is *not* a practical example of how you'll use coding in your day to day, BUT it does appear on a surprising number of in-person coding interviews.  Why employers rely on something so impractical to gauge your coding ability is beyond me.
+#       But since they're using it, maybe in this narrow sense it's a practical exercise to know how to do after all.
+
+
+# Create a program that loops through the numbers 0-100.
+# When you land on a number evenly divisible by 3, have your program print "Fizz"
+# When you land on a number evenly divisible by 5, have your program print "Buzz"
+# When you land on a number evenly divisible by both 3 and 5, have your program print "Fizzbuzz"
+# When you land on any other number, print that number
+
+# Remember the % operator will tell you whether a number is evenly divisible by another number! 
+# We use it most often to determine whether a number is even or odd, but it's also useful here.
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print "Fizzbuzz"
+    elif n % 3 == 0:
+        print "Fizz"
+    elif n % 5 == 0:
+        print "Buzz"
+    else:
+        print n
+
+# When completed, your program output should look like:
+    # 1
+    # 2
+    # Fizz
+    # 4
+    # Buzz
+    # Fizz
+    # 7
+    # 8
+    # Fizz
+    # Buzz
+    # 11
+    # Fizz
+    # 13
+    # 14
+    # Fizzbuzz
+    # 16
+    # 17
+    # Fizz
+    # 19
+    # Buzz
+    # Fizz
+    # 22
+    # 23
+    # Fizz
+    # Buzz
+    # 26
+    # Fizz
+    # 28
+    # 29
+    # Fizzbuzz
+    # 31
+    # 32
+    # Fizz
+    # 34
+    # Buzz
+    # Fizz
+    # 37
+    # 38
+    # Fizz
+    # Buzz
+    # 41
+    # Fizz
+    # 43
+    # 44
+    # Fizzbuzz
+    # 46
+    # 47
+    # Fizz
+    # 49
+    # Buzz
+    # Fizz
+    # 52
+    # 53
+    # Fizz
+    # Buzz
+    # 56
+    # Fizz
+    # 58
+    # 59
+    # Fizzbuzz
+    # 61
+    # 62
+    # Fizz
+    # 64
+    # Buzz
+    # Fizz
+    # 67
+    # 68
+    # Fizz
+    # Buzz
+    # 71
+    # Fizz
+    # 73
+    # 74
+    # Fizzbuzz
+    # 76
+    # 77
+    # Fizz
+    # 79
+    # Buzz
+    # Fizz
+    # 82
+    # 83
+    # Fizz
+    # Buzz
+    # 86
+    # Fizz
+    # 88
+    # 89
+    # Fizzbuzz
+    # 91
+    # 92
+    # Fizz
+    # 94
+    # Buzz
+    # Fizz
+    # 97
+    # 98
+    # Fizz
+    # Buzz
