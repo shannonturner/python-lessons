8f7428dcf30c0244a38c0c31793cb9625a400b42
Shannon Turner <shannon.v.turner@gmail.com>
2017-09-26 19:22:24 -0400

    Added twitter exercise

---
+++ playtime/lesson01_twitter.py
@@ -0,0 +1,23 @@
+# Difficulty Level: Beginner
+# Exercise: Tweet length calculator
+
+# Part one:
+# Create a variable called tweet and put some text in it
+#   maybe something like "Hear Me Code class was so much fun today!"
+
+# Measure the length of that tweet.
+
+# Was that tweet more than 140 characters?
+#   If so, tell the user it was too long!
+# Was that tweet 140 or fewer characters?
+#   If so, tell the user how witty they are!
+
+
+# Part two:
+# Adjust the program to say how many characters you have remaining to use, or how many you need to trim by in order to meet the 140 character limit
+
+
+# Part three:
+# Twitter announced they are changing their character limit to 280, but they might change it again.
+# Can you make your code flexible enough so that you don't have to replace the character limit in multiple places in your code?
+
